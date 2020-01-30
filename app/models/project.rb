0a4288c560d7aaa2bf68163e8b6c25cc0dd7fcd7
Ania Olak <anna.olak@lunarlogic.io>
2020-01-30 18:13:06 +0100

    Add scope to project model

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,9 @@
 class Project < ApplicationRecord
   validates_presence_of  :title, :body, :thumb_image, :main_image
+
+  def self.angular
+    where(subtitle: 'Angular')
+  end
+
+  scope :ruby_on_rails, -> { where(subtitle: 'Ruby on Rails') }
 end
